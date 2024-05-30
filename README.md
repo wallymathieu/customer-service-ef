0fc90c75e660e19bc75a9a04fad91c922de6e57b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2024-05-30 19:48:26 +0300

    Init of repository
    See https://github.com/wallymathieu/entity-framework-studies

---
+++ README.md
@@ -0,0 +1,5 @@
+# Customer Service API and site
+
+Simple customer service API and site.
+
+Split from [Entity framework studies](https://github.com/wallymathieu/entity-framework-studies).
---
+++ tests/Tests/InMemoryCustomerDataTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SomeBasicEFApp.Tests;
+
+public class InMemoryCustomerDataTests : CustomerDataTests
+{
+    private static string rnd => Guid.NewGuid().ToString("N");
+    private static Lazy<DbContextOptions> options = new(() =>
+        Setup(new DbContextOptionsBuilder()
+            .UseInMemoryDatabase(databaseName: $"customer_data_tests_{rnd}")
+            .Options));
+    public override DbContextOptions Options => options.Value;
+}
