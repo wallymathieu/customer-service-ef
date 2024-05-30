5cc745d42da7f5650b3d9691d17b6e004b59205b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2024-05-30 23:28:04 +0300

    Split into two fixtures

--- tests/Tests/ApiFixture.cs
+++ tests/Tests/ApiFixture.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,46 +11,56 @@ using SomeBasicEFApp.Web;
 using SomeBasicEFApp.Web.Data;
 
 namespace SomeBasicEFApp.Tests;
-
-public class ApiFixture:IDisposable
+public class DbFixture : IDisposable
 {
-    static TestServer Create()
-    {
-        return new TestServer(new WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .UseConfiguration(new ConfigurationBuilder().Build())
-            .UseStartup<TestStartup>()) { AllowSynchronousIO=true };
-    }
-    private readonly TestServer _testServer;
-    public ApiFixture() => _testServer = Create();
+    const string db = "ApiFixture.db";
+    public string ConnectionString => "Data Source=" + db;
     public void Dispose()
     {
-        _testServer.Dispose();
         if (!File.Exists(db)) return;
-        try{ File.Delete(db); }
+        try { File.Delete(db); }
         catch
         {
             // ignored
         }
     }
-    public TestServer Server=>_testServer;
-
-    const string db = "ApiFixture.db";
-    class TestStartup : Startup
+}
+public class ApiFixture : IDisposable
+{
+    TestServer Create()
     {
-        public TestStartup(IConfiguration configuration, IWebHostEnvironment env) : base(configuration, env)
-        {
-        }
-        protected override void ConfigureDbContext(DbContextOptionsBuilder options)
-        {
-            options.UseSqlite("Data Source=" + db);
-        }
-        protected override void OnConfigured(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
-            var context = serviceScope.ServiceProvider.GetRequiredService<CoreDbContext>();
-            context.Database.Migrate();
-        }
+        return new TestServer(new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseConfiguration(new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string?>{
+                {"ConnectionStrings:DefaultConnection",_dbFixture.ConnectionString}
+            }).Build())
+            .UseStartup<UseSqliteStartup>())
+        { AllowSynchronousIO = true };
+    }
+    private readonly TestServer _testServer;
+    private readonly DbFixture _dbFixture = new();
+    public ApiFixture() => _testServer = Create();
+    public void Dispose()
+    {
+        _testServer.Dispose();
+        _dbFixture.Dispose();
+    }
+    public TestServer Server => _testServer;
+}
+class UseSqliteStartup : Startup
+{
+    public UseSqliteStartup(IConfiguration configuration, IWebHostEnvironment env) : base(configuration, env)
+    {
+    }
+    protected override void ConfigureDbContext(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+    }
+    protected override void OnConfigured(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        var context = serviceScope.ServiceProvider.GetRequiredService<CoreDbContext>();
+        context.Database.Migrate();
     }
 }
